d1cb028bf0f9cb4d2a5209170b904034380881c7
dominicod <dominicodonnell99@gmail.com>
2022-12-14 19:16:38 -0600

    Initial Commit

---
+++ PortfolioV2.UnitTests/Systems/Controllers/TestHomeController.cs
@@ -0,0 +1,9 @@
+namespace PortfolioV2.UnitTests.Systems.Controllers;
+
+public class TestHomeController
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ PortfolioV2.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PortfolioV2.WebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
