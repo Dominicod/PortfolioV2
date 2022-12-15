fcc0d4f6cc46c1693119c5b8e29a27135e2019e7
dominicod <dominicodonnell99@gmail.com>
2022-12-14 20:11:16 -0600

    Feat: add postgres DB

---
+++ PortfolioV2.WebApp/Data/PortfolioV2Context.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using PortfolioV2.WebApp.Models;
+
+namespace PortfolioV2.WebApp.Data;
+
+public partial class PortfolioV2Context : DbContext
+{
+    public PortfolioV2Context()
+    {
+    }
+
+    public PortfolioV2Context(DbContextOptions<PortfolioV2Context> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseNpgsql("Host=localhost;Database=portfolio_v2"); // TODO: Update this connection string to be placed into secrets
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("users_pkey");
+
+            entity.Property(e => e.Id).UseIdentityAlwaysColumn();
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ PortfolioV2.WebApp/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace PortfolioV2.WebApp.Models;
+
+[Table("users")]
+[Index("Email", Name = "users_email_key", IsUnique = true)]
+public partial class User
+{
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("email")]
+    public string Email { get; set; } = null!;
+
+    [Column("role")]
+    public string Role { get; set; } = null!;
+
+    [Column("password_digest")]
+    public string PasswordDigest { get; set; } = null!;
+}
